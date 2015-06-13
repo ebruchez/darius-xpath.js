ffa60ff530c17766f1db387671b2a96ff8454bf9
ebruchez <ebruchez@orbeon.com>
2015-06-13 11:12:59 -0700

    Implement RegExp, fixes #13

--- xpath/src/org/orbeon/darius/xpath/regex/RegExp.scala
+++ xpath/src/org/orbeon/darius/xpath/regex/RegExp.scala
@@ -2,18 +2,24 @@ package org.orbeon.darius.xpath.regex
 
 import scala.util.matching.Regex
 
-class MatchResult {
-  def getGroup(i: Int): String = ???
+class MatchResult(regexMatch: Regex.Match) {
+  def getGroup(i: Int): String = {
+    regexMatch.group(i)
+  }
 }
 
 class RegExp(regex: Regex) {
 
   def exec(input: String): MatchResult = {
-//    regex.findFirstMatchIn(input).
-    ???
+    regex.findFirstMatchIn(input) match {
+      case Some(regexMatch) ⇒ new MatchResult(regexMatch)
+      case None             ⇒ null
+    }
   }
 
-  def test(input: String): Boolean = ???
+  def test(input: String): Boolean = {
+    regex.findFirstMatchIn(input).isDefined
+  }
 }
 
 object RegExp {
